24ca04ba64373720dc1020db86ffc6fd1d4f6d50
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2022-08-27 18:56:26 +0700

    #5 Setup signalR client for handle notification, message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-// routes
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as signalR from '@microsoft/signalr';
+import { setConnection } from './redux/slices/notification';
 import Router from './routes';
+
+// hooks
+import useAuth from './hooks/useAuth';
+
 // theme
 import ThemeProvider from './theme';
 // components
@@ -16,6 +23,61 @@ import MotionLazyContainer from './components/animate/MotionLazyContainer';
 // ----------------------------------------------------------------------
 
 export default function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useAuth();
+  const { connection: notificationConnection } = useSelector((state) => state.notification);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const connection = new signalR.HubConnectionBuilder()
+        .withUrl(`${process.env.REACT_APP_HOST_API_KEY}/hubs/notifications`, {
+          skipNegotiation: true,
+          transport: signalR.HttpTransportType.WebSockets,
+          accessTokenFactory: () => localStorage.getItem('accessToken'),
+          withCredentials: true,
+        })
+        .configureLogging(signalR.LogLevel.Debug)
+        .withHubProtocol(new signalR.JsonHubProtocol())
+        .withAutomaticReconnect()
+        .build();
+      connection
+        .start()
+        .then(() => {
+          dispatch(setConnection(connection));
+        })
+        .catch((error) => {
+          console.log('Connection error', error);
+        });
+    }
+  }, [isAuthenticated, dispatch]);
+
+  useEffect(() => {
+    
+    // handle recv message from signalR server
+    const handleNotification = (message) => {
+      console.log(message);
+    };
+    notificationConnection?.on('SendNotification', handleNotification);
+
+    // This code used for demo. a notication  will have invoked (sent) to signalR server each 5 minute 
+    const timer = setInterval(() => {
+      notificationConnection?.invoke('Send', {
+        To: 'customer@duckieswat.com',
+        Content: {
+          Title: "This is notifacition sent from admin",
+          Description: "hola",
+          Type: 0
+        }
+      });
+    }, 60000 * 5);
+
+    return () => {
+      // Clear timer as well as connections to avoid memory leak
+      clearTimeout(timer);
+      notificationConnection?.off('SendMessage', notificationConnection);
+    };
+  }, [notificationConnection]);
+
   return (
     <ThemeProvider>
       <ThemeColorPresets>
