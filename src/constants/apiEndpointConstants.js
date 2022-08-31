8b41cb9c9e24859faf35e4d4774360274dfdb933
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2022-08-31 17:22:03 +0700

    #11 Add change password

--- src/constants/apiEndpointConstants.js
+++ src/constants/apiEndpointConstants.js
@@ -2,3 +2,4 @@
 export const LOGIN_ENDPOINT = '/api/v1/auth/login';
 export const REGISTER_ENDPOINT = '/api/v1/auth/register';
 export const MY_PROFILE_ENDPOINT = '/api/v1/users/my-account';
+export const CHANGE_PASSWORD_ENDPOINT = '/api/v1/auth/my-account/password';
