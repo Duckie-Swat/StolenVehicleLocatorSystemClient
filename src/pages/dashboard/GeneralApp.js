969d7c883de785ea8b2393eeacb76e57030f0268
Nam Le <letranbaonam0301@gmail.com>
2022-09-18 00:51:39 +0700

    #16 Integrate mapbox to dashboard

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,16 @@
+import {useEffect, useState} from 'react';
 // @mui
 import { useTheme } from '@mui/material/styles';
 import { Container, Grid, Stack } from '@mui/material';
+// react mapbox
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
 // hooks
 import useAuth from '../../hooks/useAuth';
 import useSettings from '../../hooks/useSettings';
 // components
 import Page from '../../components/Page';
+import Label from '../../components/Label';
 // sections
 import {
   AppWidget,
@@ -19,6 +24,7 @@ import {
   AppCurrentDownload,
   AppTopInstalledCountries,
 } from '../../sections/@dashboard/general/app';
+import {MAPBOX_API} from '../../config';
 
 // ----------------------------------------------------------------------
 
@@ -27,79 +33,61 @@ export default function GeneralApp() {
   const theme = useTheme();
   const { themeStretch } = useSettings();
 
+  const [viewport, setViewport] = useState({
+    width: "70vw",
+    height: "66vh",
+    latitude: 10.82302,
+    longitude: 106.62965,
+    zoom: 14,
+  });
+
   return (
     <Page title="General: App">
-      <Container maxWidth={themeStretch ? false : 'xl'}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={8}>
-            <AppWelcome displayName={user?.displayName} />
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <AppFeatured />
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <AppWidgetSummary
-              title="Total Active Users"
-              percent={2.6}
-              total={18765}
-              chartColor={theme.palette.primary.main}
-              chartData={[5, 18, 12, 51, 68, 11, 39, 37, 27, 20]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <AppWidgetSummary
-              title="Total Installed"
-              percent={0.2}
-              total={4876}
-              chartColor={theme.palette.chart.blue[0]}
-              chartData={[20, 41, 63, 33, 28, 35, 50, 46, 11, 26]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <AppWidgetSummary
-              title="Total Downloads"
-              percent={-0.1}
-              total={678}
-              chartColor={theme.palette.chart.red[0]}
-              chartData={[8, 9, 31, 8, 16, 37, 8, 33, 46, 31]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentDownload />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppAreaInstalled />
-          </Grid>
-
-          <Grid item xs={12} lg={8}>
-            <AppNewInvoice />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopRelated />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopInstalledCountries />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTopAuthors />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <Stack spacing={3}>
-              <AppWidget title="Conversion" total={38566} icon={'eva:person-fill'} chartData={48} />
-              <AppWidget title="Applications" total={55566} icon={'eva:email-fill'} color="warning" chartData={75} />
-            </Stack>
-          </Grid>
-        </Grid>
+      <Container maxWidth={themeStretch ? false : 'xl'} sx={{display: 'flex', justifyContent: 'center',}}>
+        {/* <Grid container spacing={3}> */}
+          <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={MAPBOX_API}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            mapStyle="mapbox://styles/tonyb0301/cl7kosoop000514noo0nl4uit"
+          >
+            <Marker
+              latitude={10.82302}
+              longitude={106.62965}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <Room style={{ frontsize: viewport.zoom, color: "red" }} />
+            </Marker>
+            <Popup
+              latitude={10.82302}
+              longitude={106.62965}
+              // closeButton={true}
+              // closeOnClick={false}
+              anchor="left"
+            >
+              <div className="card">
+                <Label>Place</Label>
+                <h4 className="place">Ho Chi Minh City</h4>
+                <Label>Review</Label>
+                <p className="desc">This is a nice place</p>
+                <Label>Rating</Label>
+                <div className="stars">
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                </div>
+                <Label>Information</Label>
+                <span className="username">
+                  Created by <b>Tony</b>
+                </span>
+                <span className="date">1 hour ago</span>
+              </div>
+            </Popup>
+          </ReactMapGL>
+        {/* </Grid> */}
       </Container>
     </Page>
   );
