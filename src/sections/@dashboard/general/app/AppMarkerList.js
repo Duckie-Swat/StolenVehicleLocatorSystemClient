bb5c67cad7d46ac48fe18c774c6dacb1d8a9d40a
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2022-12-10 00:03:16 +0700

    Add display postition when display list detected results on mapbox

--- src/sections/@dashboard/general/app/AppMarkerList.js
+++ src/sections/@dashboard/general/app/AppMarkerList.js
@@ -1,12 +1,28 @@
 import { Marker, Popup } from 'react-map-gl';
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { Room } from '@material-ui/icons';
 import { fDateTimeSuffix } from '../../../../utils/formatTime';
 import Label from '../../../../components/Label';
 import Image from '../../../../components/Image';
+import { MAPBOX_API } from '../../../../config';
+import axios from '../../../../utils/axios';
 
 const AppMarkerList = ({ cameraDetectResults }) => {
-  console.log('AppMarkerList');
+  const [address, setAddress] = React.useState('');
+  useEffect(() => {
+    const fetchAddress = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${cameraDetectResults[0].longitude},${cameraDetectResults[0].latitude}.json?access_token=${MAPBOX_API}`
+        );
+        setAddress(response.data.features[0].place_name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAddress();
+  }, [cameraDetectResults]);
+
   return (
     <div>
       {cameraDetectResults?.map((cameraDetectResult) => (
@@ -25,10 +41,18 @@ const AppMarkerList = ({ cameraDetectResults }) => {
               <p>{cameraDetectResult.plateNumber}</p>
 
               <Image
-                src={`data:image/png;base64, ${cameraDetectResult.photo}`}
+                src={`data:image/jpeg;base64,${cameraDetectResult.photo}`}
                 alt="base64 img"
                 style={{ width: '150px' }}
               />
+              <Label>Position</Label>
+              <p
+                style={{
+                  width: '300px',
+                }}
+              >
+                {address}
+              </p>
               <Label>Time</Label>
               <p>{fDateTimeSuffix(cameraDetectResult.createdAt)}</p>
             </div>
